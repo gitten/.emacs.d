a67497e6cb2ecbec7b828fac3947a23be0412583
gitten <gitten@users.noreply.github.com>
2015-08-20 23:16:22 -1000

    web-mode working

--- init.el
+++ init.el
@@ -137,16 +137,16 @@
 (use-package web-mode
 	     :ensure t
 	     :config
-	     (add-to-list 'auto-mode-alist '(("\\.phtml\\'" . web-mode)
-					     ("\\.tpl\\.php\\'" . web-mode)
-					     ("\\.[agj]sp\\'" . web-mode)
-					     ("\\.as[cp]x\\'" . web-mode)
-					     ("\\.erb\\'" . web-mode)
-					     ("\\.mustache\\'" . web-mode)
-					     ("\\.djhtml\\'" . web-mode)
-					     ("\\.html?\\'" . web-mode)))
-	     (setq web-mode-engines-alist '(("django" . "\\.html\\'")))
-	     (defun my-web-mode-hook ()
+	     (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+	     (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+             (setq web-mode-engines-alist '(("django" . "\\.html\\'")))
+                          (defun my-web-mode-hook ()
 	       "Hooks for Web mode."
 	       (setq web-mode-markup-indent 2)
 	       (setq web-mode-css-indent-offset 2)
@@ -170,8 +170,7 @@
 
 
 
-
 ;;theme
 (use-package base16-theme :ensure t)
 (use-package nyan-mode :ensure t :demand t);Demand the NYAN!!
-(load "./customize-init.el")
+(load "~/.emacs.d/customize-init.el")
