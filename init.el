d98b0d9895acb0c8b29073e71d07570bc70f0a84
gitten <gitten@users.noreply.github.com>
2015-09-08 21:59:12 -1000

    abo tweaking

--- init.el
+++ init.el
@@ -54,7 +54,10 @@
 
 (use-package fish-mode :ensure t)
 
-
+(use-package key-chord
+  :ensure t
+  :config
+  (key-chord-mode 1))
 
 (use-package smartparens
   :ensure t 
@@ -92,6 +95,7 @@
   :load-path "sdic-2.1.3/")
 
 ;;;; abo fun time
+(use-package avy :ensure t)
 
 (use-package hydra :ensure t :demand t)
 
@@ -107,51 +111,26 @@
 (use-package ace-window
           :ensure t
           :config
+          (winner-mode t)
           (set-face-attribute 'aw-leading-char-face nil
                               :foreground "deep sky blue"
                               :weight 'bold
                               :height 3.0)
-          (set-face-attribute 'aw-mode-line-face nil
-                              :inherit 'mode-line-buffer-id
-                              :foreground "lawn green")
-          (setq aw-keys   '(?a ?s ?d ?f ?j ?k ?l)
-                aw-dispatch-always t
-                aw-dispatch-alist
-                '((?x aw-delete-window     "Ace - Delete Window")
-                  (?c aw-swap-window       "Ace - Swap Window")
-                  (?n aw-flip-window)
-                  (?v aw-split-window-vert "Ace - Split Vert Window")
-                  (?h aw-split-window-horz "Ace - Split Horz Window")
-                  (?m delete-other-windows "Ace - Maximize Window")
-                  (?g delete-other-windows)
-                  (?b balance-windows)
-                  (?u winner-undo)
-                  (?r winner-redo)))
+          (setq aw-keys '(?a ?s ?d ?f ?j ?k ?l)
+                aw-dispatch-always t)
+          )
 
-          (when (package-installed-p 'hydra)
-            (defhydra hydra-window-size (:color red)
-              "Windows size"
-              ("h" shrink-window-horizontally "shrink horizontal")
-              ("j" shrink-window "shrink vertical")
-              ("k" enlarge-window "enlarge vertical")
-              ("l" enlarge-window-horizontally "enlarge horizontal"))
-            (defhydra hydra-window-frame (:color red)
-              "Frame"
-              ("f" make-frame "new frame")
-              ("x" delete-frame "delete frame"))
-            (defhydra hydra-window-scroll (:color red)
-              "Scroll other window"
-              ("n" joe-scroll-other-window "scroll")
-              ("p" joe-scroll-other-window-down "scroll down"))
-            (add-to-list 'aw-dispatch-alist '(?w hydra-window-size/body) t)
-            (add-to-list 'aw-dispatch-alist '(?o hydra-window-scroll/body) t)
-            (add-to-list 'aw-dispatch-alist '(?\; hydra-window-frame/body) t))
-          (ace-window-display-mode t))
+
+;                  (?b balance-windows)
+ ;                 (?u winner-undo)
+  ;                (?r winner-redo)))
+
 
 (defhydra hydra-zoom (global-map "<f2>")
   "zoom"
   ("g" text-scale-increase "in")
-  ("l" text-scale-decrease "out"))
+  ("l" text-scale-decrease "out")
+  ("o" nil "exit"))
 
 
 
@@ -166,7 +145,7 @@
   ("s" (lambda () (interactive) (ace-window 4)) "swap")
   ("d" (lambda () (interactive) (ace-window 16)) "delete")
   ("o" nil "exit"))
-(use-package key-chord :ensure t :config (key-chord-mode 1))
+
 (key-chord-define-global "yy" 'hydra-window/body)
 
 
