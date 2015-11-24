e9476debf5bf676fb90fd46c271b120adaa8bc1a
gitten <gitten@users.noreply.github.com>
2015-11-23 22:53:08 -1000

    orgified my config

--- customize-init.el
+++ customize-init.el
@@ -3,10 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (base16-apathy-dark)))
+ '(ansi-color-names-vector
+   ["#28211c" "#cf6a4c" "#54be0d" "#f9ee98" "#5ea6ea" "#9b859d" "#5ea6ea" "#8a8986"])
+ '(ansi-term-color-vector
+   [unspecified "#28211c" "#cf6a4c" "#54be0d" "#f9ee98" "#5ea6ea" "#9b859d" "#5ea6ea" "#8a8986"])
+ '(custom-enabled-themes (quote (base16-bright-dark)))
  '(custom-safe-themes
    (quote
-    ("17f35b689dd41e49cb740bfb810ac8a53d13292cbebf68f41f772787d8b3aebf" default)))
+    ("0bd7a42bd443517e5e61dac3cabc24018fbd0c6b2b4199b3c4efd9e3727efd30" "8e3f020f1ce69cfa0c1ebee4e198feb28dd7eb31b7d77927e9c790819210c654" "6ae93caf30ad7eef728589a4d7b7befadecade71d78b904a64a0480608a7b61e" "a7b47876e5da7cac6f5e61cca7a040a365ca2c498823654bd4076add8edf34c5" "d1a42ed39a15a843cccadf107ee0242b5f78bfbb5b70ba3ce19f3ea9fda8f52d" "91fba9a99f7b64390e1f56319c3dbbaed22de1b9676b3c73d935bf62277b799c" "1edf370d2840c0bf4c031a044f3f500731b41a3fd96b02e4c257522c7457882e" "cb18233197cedab557c70d171b511bed49cc702f428750925280090c31498bd2" "d43120398682953ef18fd7e11e69c94e44d39bb2ab450c4e64815311542acbff" "294834baa9ca874795a3181cce7aaf228b1e3fb3899587ffd3ae7546de328c90" "db9feb330fd7cb170b01b8c3c6ecdc5179fc321f1a4824da6c53609b033b2810" "17f35b689dd41e49cb740bfb810ac8a53d13292cbebf68f41f772787d8b3aebf" default)))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote cabal-repl))
  '(paradox-github-token t)
  '(tool-bar-mode nil))
 (custom-set-faces
