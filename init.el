66f26cfef6719d51c9cd31f25ca7ec13d7088428
gitten <gitten@users.noreply.github.com>
2017-07-09 00:16:08 -1000

    init minimal config

---
+++ init.el
@@ -0,0 +1,36 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+(use-package ivy :ensure t
+  :config (ivy-mode 1))
+(use-package counsel
+  :ensure t
+  :bind ("C-x C-f" . counsel-find-file))
+
+(use-package swiper :ensure t)
+
+(use-package magit :ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit counsel ivy use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
